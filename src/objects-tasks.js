df79137768a61f28bc7d888fc7302dd1506870bd
arsenyoung <molodoiars@gmail.com>
2024-03-07 09:10:47 +0400

    feat: makeword implementation

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -182,8 +182,23 @@ function makeImmutable(obj) {
  *    makeWord({ a: [0, 1], b: [2, 3], c: [4, 5] }) => 'aabbcc'
  *    makeWord({ H:[0], e: [1], l: [2, 3, 8], o: [4, 6], W:[5], r:[7], d:[9]}) => 'HelloWorld'
  */
-function makeWord(/* lettersObject */) {
-  throw new Error('Not implemented');
+function makeWord(lettersObject) {
+  if (!lettersObject || typeof lettersObject !== 'object') {
+    return null;
+  }
+
+  const wordObject = {};
+  const lettersObjectEntries = Object.entries(lettersObject);
+  for (let i = 0; i < lettersObjectEntries.length; i += 1) {
+    const [letter, keys] = lettersObjectEntries[i];
+    for (let j = 0; j < keys.length; j += 1) {
+      const key = keys[j];
+      wordObject[key] = letter;
+    }
+  }
+  const wordObjectValues = Object.values(wordObject);
+
+  return wordObjectValues.join('');
 }
 
 /**
