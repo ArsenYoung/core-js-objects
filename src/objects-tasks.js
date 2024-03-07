f42b573e3d3fc5666bab44d04f027ce5b91dd78f
arsenyoung <molodoiars@gmail.com>
2024-03-07 12:04:10 +0400

    feat: fromjson implementation

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -299,8 +299,10 @@ function getJSON(obj) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  const resultObj = JSON.parse(json);
+  Object.setPrototypeOf(resultObj, proto);
+  return resultObj;
 }
 
 /**
