ac624b64185ef15116aa6619d312850dcca19b74
arsenyoung <molodoiars@gmail.com>
2024-03-07 10:11:33 +0400

    feat: rectangle implementation

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -253,8 +253,25 @@ function sellTickets(queue) {
  *    console.log(r.height);      // => 20
  *    console.log(r.getArea());   // => 200
  */
-function Rectangle(/* width, height */) {
-  throw new Error('Not implemented');
+
+class Rect {
+  width;
+
+  height;
+
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+  }
+
+  getArea() {
+    return this.width * this.height;
+  }
+}
+
+function Rectangle(width, height) {
+  const rectangle = new Rect(width, height);
+  return rectangle;
 }
 
 /**
